619eed72fc325677fb815f70ccd5ea5cf1a20ef5
abedin wahid <abedinwahid9@gmail.com>
2024-04-11 20:26:04 +0500

    my cart page design complete

--- src/components/MyCart/MyCart.js
+++ src/components/MyCart/MyCart.js
@@ -9,11 +9,73 @@ const MyCart = () => {
     <>
       <div className="flex justify-center items-center">
         <Title title="my" />
-        <Lottie className="w-24 h-24" animationData={mycart} loop={true} />
+        <Lottie
+          className="md:w-24 md:h-24 w-16 h-16"
+          animationData={mycart}
+          loop={true}
+        />
         <Title title="cart" />
       </div>
-      <div>
-        <MyCartCard />
+      <div className="flex flex-col md:flex-row gap-5 md:my-10 my-5 ">
+        <div className="md:w-2/3 w-full">
+          <h3 className="my-3 font-lobsterTwo font-semibold text-xl dark:text-secondaryColor">
+            Total Cart Items: 6
+          </h3>
+          <div className="  flex flex-col gap-5">
+            <MyCartCard />
+            <MyCartCard />
+            <MyCartCard />
+            <MyCartCard />
+          </div>
+        </div>
+        <div className="md:w-1/3 w-full ">
+          <h3 className="my-3 uppercase text-center font-lobsterTwo font-semibold text-xl dark:text-secondaryColor">
+            order summery
+          </h3>
+          <div className="flex flex-col gap-5">
+            <div className="flex flex-col gap-3">
+              <div className="flex justify-between">
+                <div className="flex flex-col gap-3">
+                  <div>
+                    <p className="text-base capitalize font-semibold dark:text-secondaryColor">
+                      Sub Total:
+                    </p>
+                    <p className="text-base capitalize font-semibold dark:text-secondaryColor">
+                      delivery Charge:
+                    </p>
+                  </div>
+                </div>
+                <div>
+                  <div>
+                    <p className="text-base capitalize font-semibold dark:text-secondaryColor">
+                      ৳ 200
+                    </p>
+                    <p className="text-base capitalize font-semibold dark:text-secondaryColor">
+                      ৳ 300
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <hr />
+              <div className="flex justify-between">
+                <p className="text-base capitalize font-semibold dark:text-secondaryColor">
+                  Total:
+                </p>
+                <p className="text-base capitalize font-semibold dark:text-secondaryColor">
+                  ৳ 600
+                </p>
+              </div>
+            </div>
+            <div className="flex flex-col gap-2">
+              <button className="text-lg py-2 rounded-sm px-4 bg-optionalColor font-semibold uppercase">
+                continue shopping
+              </button>
+              <button className="text-lg py-2 rounded-sm px-4 bg-red-500 font-semibold uppercase">
+                cancle
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
--- src/components/MyCart/MyCartCard/MyCartCard.js
+++ src/components/MyCart/MyCartCard/MyCartCard.js
@@ -1,7 +1,82 @@
+"use client";
+import Image from "next/image";
+import singlePic from "@/assets/pickle.jpg";
+import { useState } from "react";
+import { MdDelete } from "react-icons/md";
+
 const MyCartCard = () => {
+  const [input, setInput] = useState(1);
+  const stock = 6;
+
+  const handleInput = (e) => {
+    setInput(e.target.value);
+  };
+
+  const handleIncrement = () => {
+    if (input < stock) {
+      setInput(input + 1);
+    }
+  };
+
+  const handleDecrement = () => {
+    if (input > 1) {
+      setInput(input - 1);
+    }
+  };
   return (
-    <div>
-      <h2>mycart</h2>
+    <div className="flex justify-between flex-col gap-3 md:flex-row drop-shadow-sm  border-2  p-5 ">
+      <div className="flex gap-5">
+        <Image
+          className="md:w-32 w-20 md:h-32 h-20 rounded-sm object-cover"
+          src={singlePic}
+          width={0}
+          height={0}
+          alt="mycartimg"
+        />
+        <div className="flex flex-col justify-between">
+          <div>
+            <h3 className="md:text-xl text-sm font-bold dark:text-secondaryColor uppercase">
+              products name
+            </h3>
+
+            <p className="md:text-base text-sm font-bold dark:text-secondaryColor">
+              Price: 350
+            </p>
+          </div>
+          <div className="flex gap-2 ">
+            <button
+              disabled={stock === 0}
+              onClick={handleDecrement}
+              className="bg-optionalColor disabled:opacity-75 md:text-2xl text-lg font-bold md:w-10 md:h-10 w-7 h-7 disabled"
+            >
+              -
+            </button>
+            <input
+              disabled
+              onChange={handleInput}
+              className="md:w-10 md:h-10 w-7 h-7 text-center focus:outline-none disabled:bg-white"
+              type="number"
+              name=""
+              id=""
+              value={input}
+            />
+            <button
+              disabled={stock === 0}
+              onClick={handleIncrement}
+              className="bg-optionalColor disabled:opacity-75 md:text-2xl text-lg font-bold md:w-10 md:h-10 w-7 h-7"
+            >
+              +
+            </button>
+          </div>
+        </div>
+      </div>
+
+      <div className="flex md:flex-col flex-row justify-between md:items-end items-start">
+        <p className="text-base font-bold dark:text-secondaryColor">
+          Totol Price: {350 * input}
+        </p>{" "}
+        <MdDelete className="w-7 h-7 cursor-pointer" />
+      </div>
     </div>
   );
 };
